46a4b24f2471f94a249d3dfc514f1f4c20bea8f3
Lakecityultrabots8041 <122642867+Lakecityultrabots8041@users.noreply.github.com>
2025-02-10 20:37:55 -0500

    Refactor ElevatorIO interface to class and remove unused methods

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
@@ -1,29 +1,5 @@
-package frc.robot.subsystems.elevator;
+package frc.robot.subsystems.Elevator;
 
-public interface ElevatorIO {
-
-    /**
-     * Updates the current elevator inputs (position, velocity, voltage, etc.).
-     */
-    void updateInputs(ElevatorIOInputs inputs);
-
-    /**
-     * Runs the elevator to a specific setpoint position (in meters).
-     */
-    void runSetpoint(double targetPositionMeters);
-
-    /**
-     * Runs the elevator using the specified voltage.
-     */
-    void runVolts(double volts);
-
-    /**
-     * Sets the PID gains dynamically.
-     */
-    void setPID(double p, double i, double d);
-
-    /**
-     * Stops the elevator.
-     */
-    void stop();
+public class ElevatorIO {
+    
 }
